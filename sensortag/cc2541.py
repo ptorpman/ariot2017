5e6ad015388a516906e525f7d0685e8f4ba4fe89
Hasan Mahmood <hasan.mahmood@altran.com>
2017-03-10 15:38:35 +0100

    adding more sensors

--- sensortag/cc2541.py
+++ sensortag/cc2541.py
@@ -38,6 +38,49 @@ class TempAndHumidity(object):
 		self._temp_value = self._tag.IRtemperature.read()
 
 		return self._temp_value[0]
+	
+	def read_barometer(self):
+		self._tag.barometer.enable()
+		time.sleep(1.0)
+		self._baro_value = self._tag.barometer.read()
+
+		return self._baro_value[1]
+		
+	def read_accelerometer(self):
+		self._tag.accelerometer.enable()
+		time.sleep(1.0)
+		self._acce_value = self._tag.accelerometer.read()
+
+		return self._acce_value
+		
+	def read_gyroscope(self):
+		self._tag.gyroscope.enable()
+		time.sleep(1.0)
+		self._gero_value = self._tag.gyroscope.read()
+
+		return self._gero_value
+		
+	def read_magnetometer(self):
+		self._tag.magnetometer.enable()
+		time.sleep(2.0)
+		self._magn_value = self._tag.magnetometer.read()
+		return self._magn_value
+	
+	def read_door_status(self):
+		x_axis=self.read_magnetometer()[2]
+		#door_status=0 means door is closed
+		#dorr_status=1 means door is open
+		if x_axis >-47:
+			door_status = 0
+		else:
+			door_status = 1
+		return door_status
+	
+		
+	
+		
+		
+		
 
 
 		
@@ -75,7 +118,11 @@ tah = TempAndHumidity(mac)
 print "IRtemp is " , tah.read_irtemperature()
 print "Airtemp is " , tah.read_airtemperature()
 print "Humidity is ", tah.read_humidity()
-
+print "preasure is ", tah.read_barometer()
+print "accelerometer is ", tah.read_accelerometer()
+print "coordinates are ", tah.read_gyroscope()
+print "magnetometer is  ", tah.read_magnetometer()
+print "door_status is ", tah.read_door_status()
 
 
 # Sensortag
