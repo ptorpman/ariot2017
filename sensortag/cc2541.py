896a3169091b27f847e3de303d6c8f34ed2c5e3f
Hasan Mahmood <hasan.mahmood@altran.com>
2017-03-10 09:51:42 +0100

    adding sensor code for cc2541

---
+++ sensortag/cc2541.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# Code to read the sensors and control the environment for the the best conditions to our greenhouse plants.
+
+import time
+import sys
+#sys.path.append('/usr/local/lib/python2.7/dist-packages/bluepy')
+from bluepy import btle
+from bluepy import sensortag
+
+class TempAndHumidity(object):
+	def __init__(self, mac):
+		''' Constructor '''
+		print "* Connecting to sensor..."
+		self._tag = sensortag.SensorTag(addr=mac)
+		print "* Connected: ", self._tag
+		self._tag.IRtemperature.enable()
+		time.sleep(1.0)
+	
+		self._temp_value = 0.0
+
+	def read_temperature(self):
+		self._temp_value = self._tag.IRtemperature.read()
+		
+		return self._temp_value
+
+
+		
+		
+
+def read_sensor_values(tag):
+	#if sensor==1:
+	#read IRTemp
+	tag.IRtemperature.enable()
+	value=tag.IRtemperature.read()
+	print value
+	return value
+	#lif sensor==2:
+	#	#read AmbTemp
+	#	value = sensortag(mac, '-T', '-n 1')
+	#	return value[1][1]
+	#elif sensor==3:
+	#	#read Humidity
+	#	value = sensortag(mac, '-H', '-n 1')
+	#	return value[1][1]
+	#else:
+	#	#error "given value not reqognized"
+	#	print "error value not recognized"
+	#return
+
+#print "Instantiating..."
+#tag = 
+#read_sensor_values(tag)
+
+mac = 'BC:6A:29:AC:53:91'
+
+# Instantiate..
+tah = TempAndHumidity(mac)
+
+print tah.read_temperature()
+print tah.read_temperature()
+
+
+
+# Sensortag
+
+# Air temperature sensor reading
+
+
+# Control of the air temperature (turning the fan on and off)
+
+
+
+# Ground temperature sensor reading
+# NO CONTROL!!
+
+
+
+# Air humidity sensor reading
+# NO CONTROL!!
+
+
+
+
+
+
+
+
+
+# Light sensor reading 
+# Control of the light (turning the pump on and off)
+
+# WHEN??
+
+
+
+# Ground humidity sensor reading
+# Control of the ground humidity (turning the water on and off)
+
+
+
+# Water level in the can reading
+
+# TOP OR BOTTOM??
+
+
+
+# Camera capture in the mobile app!
+
+
+
+
+
+
+
+
+
+
